7b35ca4a09ea0d0ef42deacb1d9a88407b0bb4a4
endepointe <endepointe@gmail.com>
2024-03-31 14:43:26 -0700

    wasmstartover

--- src/lib/components/dashboard.svelte
+++
@@ -1,7 +0,0 @@
-<!-- dashboard.svelte -->
-<script lang="ts">
-</script>
-<h1>fullscreen menu here</h1>
-<style>
-</style>
-
--- src/lib/store.ts
+++
@@ -1,5 +0,0 @@
-import { writable } from 'svelte/store';
-
-export const count = writable(0);
-export const state_intro = writable(0);
-
