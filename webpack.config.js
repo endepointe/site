78910e68b4a355678097b0295ed3db1de2022341
endepointe <endepointe@gmail.com>
2024-06-13 09:21:00 -0700

    ssdd

--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,7 @@ module.exports = {
             },
             {
                 test: /\.js$/,
+                include: path.resolve(__dirname, 'src'),
                 exclude: /node_modules/,
                 use: {
                     loader: 'babel-loader',
