b46b048e870270ec2e1b6e569d73fb5066cf8182
endepointe <endepointe@gmail.com>
2024-06-24 14:26:47 -0700

    added sass support

--- webpack.config.js
+++ webpack.config.js
@@ -25,6 +25,14 @@ module.exports = {
                     }
                 }
             },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader',
+                ],
+            },
         ],
     },
     mode,
