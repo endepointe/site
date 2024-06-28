7879ffdb0fddf06c3193c12615426a78f3bbbde6
endepointe <endepointe@gmail.com>
2024-06-28 13:50:21 -0700

    save

--- src/slidecontrol.js
+++ src/slidecontrol.js
@@ -1,4 +1,18 @@
 
-
+const slideItems = document.getElementById('slide-list-items').children;
 const slides = document.querySelectorAll('.slide');
-console.log(slides);
+let currentSlide = 0;
+
+for (let i = 0; i < slideItems.length; i++) 
+{
+    slideItems[i].addEventListener('click', () => {
+        goToSlide(i);
+    });
+}
+
+function goToSlide(n) 
+{
+    slides[currentSlide].classList.add('visually-hidden');
+    slides[n].classList.remove('visually-hidden');
+    currentSlide = n;
+}
