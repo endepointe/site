75833a42e0dd413dae595eee304d80259e538e88
endepointe <endepointe@gmail.com>
2024-11-03 22:20:11 -0800

    prod consideration

--- src/index.js
+++ src/index.js
@@ -1,16 +1,46 @@
 import React from 'react';
 import {createRoot} from 'react-dom/client';
+import {
+    createBrowserRouter,
+    RouterProvider,
+    Route,
+    Link,
+    Navigate,
+    redirect,
+} from 'react-router-dom';
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: (
+            <div>
+                <Link to="/writeups">Writeups</Link>
+            </div>),
+    },
+    {
+        path: "/writeups",
+        element: <div>Writeups</div>,
+    },
+    {
+        path: '/writeups/',
+        element: <Navigate to="/writeups" replace />,
+    },
+]);
 
 function Contact() 
 {
     return <div><a href="google.com">ggoogle</a><h1>React Contact</h1></div>;
 }
 
-const domNode = document.getElementById('contact');
+const rootNode = document.getElementById('root');
 
-if (domNode) {
-    const root = createRoot(domNode);
-    root.render(<Contact />);
+if (rootNode) {
+    const root = createRoot(rootNode);
+    root.render(
+        <React.StrictMode>
+            <RouterProvider router={router} />
+        </React.StrictMode>
+    );
 } else {
-    console.error("there is an issue creating reactdom");
+    console.error("there is an issue creating root node");
 }
